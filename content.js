1d5039d6a8bfacba02279c10d5360d57bf4973f2
Uolary <uolary@gmail.com>
2024-10-22 18:42:58 +0200

    feature: add styles

--- content.js
+++ content.js
@@ -31,21 +31,29 @@ const copyToClipboard = (url, title) => {
 };
 
 const extensionBtn = `
-  <div role="button" aria-label="Copy task link" class="alc-extension-btn IconButtonThemeablePresentation--isEnabled IconButtonThemeablePresentation IconButtonThemeablePresentation--medium SubtleIconButton--standardTheme SubtleIconButton TaskPaneToolbar-button TaskPaneToolbar-copyLinkButton" tabindex="0">
-    <svg viewBox="0 0 24 24" fill="none" class="Icon LinkIcon" focusable="false">
+  <div role="button" aria-label="Copy task link" class="alc-extension_btn" tabindex="0">
+    <svg viewBox="0 0 24 24" fill="none" class="alc-extension_btn-icon" focusable="false">
       <path stroke="#6d6e6f" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="M17.5 14H19C20.1046 14 21 13.1046 21 12V5C21 3.89543 20.1046 3 19 3H12C10.8954 3 10 3.89543 10 5V6.5M5 10H12C13.1046 10 14 10.8954 14 12V19C14 20.1046 13.1046 21 12 21H5C3.89543 21 3 20.1046 3 19V12C3 10.8954 3.89543 10 5 10Z" />
     </svg>
   </div>
 `;
 
+const toast = `
+  <div class="alc-extension_toast alc-extension_toast__enter">
+    <div class="alc-extension_toast-container" role="alert" tabindex="-1">
+      <div class="alc-extension_toast-text">Link copied</div>
+    </div>
+  </div>
+`;
+
 waitForElement('.TaskPaneToolbar.TaskPane-header', (el) => {
-  if (!el.querySelector('.alc-extension-btn')) {
+  if (!el.querySelector('.alc-extension_btn')) {
     const firstOptionElement = el.querySelector('.SubtleHeartButton.TaskPaneToolbar-button');
 
     firstOptionElement.insertAdjacentHTML("beforebegin", extensionBtn);
 
-    el.querySelector('.alc-extension-btn').addEventListener('click', () => {
-      const svgPath = el.querySelector('.alc-extension-btn path');
+    el.querySelector('.alc-extension_btn').addEventListener('click', () => {
+      const svgPath = el.querySelector('.alc-extension_btn path');
 
       const taskUrl = window.location.href;
       const taskTitle = document.querySelector('.simpleTextarea[aria-label="Task Name"]').value;
@@ -54,8 +62,18 @@ waitForElement('.TaskPaneToolbar.TaskPane-header', (el) => {
 
       svgPath.style.stroke = '#58a182';
 
+      document.querySelector('.DefaultToastGroup').insertAdjacentHTML('afterbegin', toast);
+
       setTimeout(() => {
-        svgPath.style.stroke = '#6d6e6f';
+        svgPath.style.stroke = '#6d6e6f'
+        const toastElement = document.querySelector('.DefaultToastGroup').querySelector('.alc-extension_toast');
+
+        toastElement.classList.remove('alc-extension_toast__enter');
+        toastElement.classList.add('alc-extension_toast__exit');
+
+        setTimeout(() => {
+          toastElement.remove();
+        }, 1000);
       }, 5000);
     });
   }

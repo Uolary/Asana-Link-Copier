27214df9ff0581b829a38b22a83fd6102d288e58
Uolary <uolary@gmail.com>
2026-09-10 11:00:41 +0200

    fix: use area based selectors for task title

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -57,10 +57,14 @@ waitForElement('.TaskPaneToolbar.TaskPane-header', (el) => {
       const iconPath = el.querySelector('.alc-extension__icon-path');
 
       const taskUrl = window.location.href;
-      let taskTitle = document.querySelector('.TitleInput-objectName .SimpleTextarea')?.value;
+      let taskTitle = document.querySelector('textarea[aria-label="Task Name"]')?.value?.trim();
 
       if (!taskTitle) {
-        taskTitle = document.querySelector('.TaskPaneTitle .ReadOnlyTitleInput-name')?.textContent;
+        taskTitle = document.querySelector('[role="heading"][aria-level="2"]')?.textContent?.trim();
+      }
+
+      if (!taskTitle) {
+        taskTitle = taskUrl.match(/\/task\/(\d+)/)?.[1] || taskUrl;
       }
 
       copyToClipboard(taskUrl, taskTitle, event.shiftKey);
